0b249a6128587a13dc2ae0008ec701b40ed7e683
CesarFever <67882218+CesarFever@users.noreply.github.com>
2020-07-05 21:44:51 -0400

    Updated Movies

--- js/main.js
+++ js/main.js
@@ -32,10 +32,10 @@ function buildList()
 }
 
 
-addMovie("test", 1995, "michel bay", "some link");
-addMovie("test", 1995, "michel bay", "some link");
-addMovie("test", 1995, "michel bay", "some link");
-addMovie("test", 1995, "michel bay", "some link");
-addMovie("test", 1995, "michel bay", "some link");
+addMovie("Rush Hour", 1998, "Brett Ratner", "https://i0.wp.com/www.oicvintage.com/wp-content/uploads/2018/03/20870.jpg?w=1080&ssl=1");
+addMovie("Spongebob Squarepants Movie", 2004, "Stephen Hillenburg", "https://www.gstatic.com/tv/thumb/v22vodart/35120/p35120_v_v8_aa.jpg");
+addMovie("Toy Story", 1995, "John Lasseter", "https://upload.wikimedia.org/wikipedia/en/thumb/1/13/Toy_Story.jpg/220px-Toy_Story.jpg");
+addMovie("Big Time Movie", 2012, "Savage Steve Holland", "https://www.gstatic.com/tv/thumb/v22vodart/9073846/p9073846_v_v8_aa.jpg");
+addMovie("Toy Story 2", 1999, "John Lasseter", "https://upload.wikimedia.org/wikipedia/en/c/c0/Toy_Story_2.jpg");
 
 buildList();
